9aae2bec866b756b610e39d2ca3881fd4ced3c2e
motalibpathan11 <73628059+motalibpathan11@users.noreply.github.com>
2022-04-04 11:38:21 +0600

    navbar complete

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="bg-gray-900">
-      <div className="md:container mx-auto p-5">
+      <div className="md:container mx-auto p-5 flex justify-between items-center">
         <h1 className="text-3xl text-white font-bold">
           <span className="text-rose-500">Acer</span> Laptop Review
         </h1>
-        <nav></nav>
+        <nav className="text-white flex gap-7">
+          <Link to="/">HOME</Link>
+          <Link to="/reviews">REVIEWS</Link>
+          <Link to="/dashboard">DASHBOARD</Link>
+          <Link to="/blogs">BLOGS</Link>
+          <Link to="/about">ABOUT</Link>
+        </nav>
       </div>
     </div>
   );
